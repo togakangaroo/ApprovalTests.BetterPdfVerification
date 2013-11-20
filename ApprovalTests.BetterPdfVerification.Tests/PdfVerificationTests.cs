465a48b7e039ab9aa8c9aa091c1002e9769fd7c9
George Mauer <gmauer@gmail.com>
2013-11-20 01:56:45 -0600

    cleanup and some basic docs

--- ApprovalTests.BetterPdfVerification.Tests/PdfVerificationTests.cs
+++ ApprovalTests.BetterPdfVerification.Tests/PdfVerificationTests.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using ApprovalTests.Reporters;
-using ApprovalTests.Writers;
-using ApprovalUtilities.Utilities;
+﻿using ApprovalTests.Reporters;
 using PdfSharp.Drawing;
 using PdfSharp.Pdf;
-using PdfSharp.Pdf.IO;
 using Xunit;
 using System.IO;
 
@@ -41,73 +32,6 @@ namespace ApprovalTests.BetterPdfVerification.Tests
             var ms = new MemoryStream();
             document.Save(ms, closeStream: false);
             return ms;
-        }
-
-    }
-
-    public static class PdfApprovals
-    {
-        public static void Verify(string file) {
-            verify(PdfReader.Open(file, PdfDocumentOpenMode.Modify));
-        }
-        public static void Verify(Stream pdf) {
-            rewind(pdf);
-            verify(PdfReader.Open(pdf, PdfDocumentOpenMode.Modify));
-        }
-
-        static void verify(PdfDocument doc) {
-            var knownDate = new DateTime(2010, 1, 1);
-            doc.Info.CreationDate = doc.Info.ModificationDate = knownDate;
-            using (var ms = new MemoryStream()) {
-                doc.Save(ms, closeStream: false);
-                rewind(ms);
-                clearId(ms);
-                Approvals.VerifyBinaryFile(ms.ToArray(), "pdf");
-            }
-        }
-
-        static void rewind(Stream stream) { stream.Seek(0, SeekOrigin.Begin);}
-
-        /// <summary>
-        /// Ids look like this:
-        /// /ID[<7B8D63B6A42B804386D5145B251346BC><952D5CBCFB77E14A80CFFC76AC1D2F6A>]
-        /// </summary>
-        static void clearId(MemoryStream ms) {
-            var sr = new StreamReader(ms);
-            var enc = sr.CurrentEncoding;
-            var toMatch = enc.GetBytes("/ID[<").Concat(wildcardBytes(32)).Concat(enc.GetBytes("><")).Concat(wildcardBytes(32)).Concat(enc.GetBytes(">]")).ToArray();
-            var matchLocation = find(toMatch, ms);
-            if (matchLocation < 0)
-                return;
-            ms.Seek(matchLocation, SeekOrigin.Begin);
-            var replaceWith = enc.GetBytes("/ID[<").Concat(aBytes(32)).Concat(enc.GetBytes("><")).Concat(aBytes(32)).Concat(enc.GetBytes(">]")).ToArray();
-            ms.Write(replaceWith, 0, replaceWith.Length);
-        }
-
-        static IEnumerable<byte> wildcardBytes(int count) { return Enumerable.Repeat(default(byte), count); }
-        static IEnumerable<byte> aBytes(int count) { return Enumerable.Repeat((byte)'A', count); } 
-
-        static long find(byte[] toMatch, Stream stream) {
-            while (stream.Length != stream.Position) {
-                var initialPosition = stream.Position;
-                stream.ReadByte();
-                if (isMatchingAtCurrentPosition(toMatch, stream))
-                    return initialPosition;
-                stream.Seek(initialPosition, SeekOrigin.Begin);
-                stream.ReadByte();
-            }
-            return -1;
-        }
-
-        static bool isMatchingAtCurrentPosition(byte[] toMatch, Stream stream) {
-            int idx;
-            stream.Seek(-1, SeekOrigin.Current);
-            for (idx = 0; idx < toMatch.Length && stream.Position < stream.Length && compare(toMatch[idx], stream.ReadByte()); idx++ ) { }
-            return idx == toMatch.Length;
-        }
-
-        static bool compare(byte matchingByte, int targetByte) {
-            return matchingByte == default(byte) || matchingByte == targetByte;
         }
     }
 }
